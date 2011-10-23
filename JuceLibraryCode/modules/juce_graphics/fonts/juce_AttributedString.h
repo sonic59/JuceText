5ae2253a9047e36c32234d0d6552cef436daa0fd
Sonic Fiftynine <supermario30@gmail.com>
2011-10-23 13:46:36 -0400

    Added default constructors to Attribute classes. Added leak detector to Attribute classes.

--- JuceLibraryCode/modules/juce_graphics/fonts/juce_AttributedString.h
+++ JuceLibraryCode/modules/juce_graphics/fonts/juce_AttributedString.h
@@ -29,6 +29,7 @@
 class JUCE_API  Attr
 {
 public:
+    Attr() {}
     virtual ~Attr() {}
 
     enum Attribute
@@ -44,42 +45,62 @@ public:
 
     Attribute attribute;
     Range<int> range;
+private:
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (Attr);
 };
 
 class JUCE_API  AttrColour : public Attr
 {
 public:
+    AttrColour() {}
     Colour colour;
+private:
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AttrColour);
 };
 
 class JUCE_API  AttrFont : public Attr
 {
 public:
+    AttrFont() {}
     Font font;
+private:
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AttrFont);
 };
 
 class JUCE_API  AttrString : public Attr
 {
 public:
+    AttrString() {}
     String text;
+private:
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AttrString);
 };
 
 class JUCE_API  AttrFloat : public Attr
 {
 public:
+    AttrFloat() {}
     float value;
+private:
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AttrFloat);
 };
 
 class JUCE_API  AttrInt : public Attr
 {
 public:
+    AttrInt() {}
     int value;
+private:
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AttrInt);
 };
 
 class JUCE_API  AttrBool : public Attr
 {
 public:
+    AttrBool() {}
     bool value;
+private:
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AttrBool);
 };
 
 class JUCE_API  AttributedString
